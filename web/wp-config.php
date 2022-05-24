3b27ba462b969df527a8ac9464c9c4220e43d47a
Anna Cunnane <acunnane13@gmail.com>
2022-05-24 17:25:30 +0100

    remove display of deprecated notices

--- web/wp-config.php
+++ web/wp-config.php
@@ -7,3 +7,4 @@
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 require_once dirname(__DIR__) . '/config/application.php';
 require_once ABSPATH . 'wp-settings.php';
+error_reporting(E_ALL ^ E_DEPRECATED);
