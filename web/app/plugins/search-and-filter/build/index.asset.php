a3bc110482e0f0cc18f2700017eeb4531a764e79
Moggach <acunnane13@gmail.com>
2023-11-27 14:11:42 +0000

    take search out of block name

--- web/app/plugins/search-and-filter/build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks'), 'version' => 'eabecc819c753596976f');
--- web/app/plugins/search-and-filter/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '337f5d1e1b405573e5c0');
